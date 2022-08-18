5787d70978f49e4defb50fe230eb902599824ef2
Sam Karp <sfcypret@gmail.com>
2022-08-17 20:51:34 -0400

    port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 const {animals} = require('./data/animals');

6452841d864deb441a25d3de94d2833c07d1108c
Sam Karp <sfcypret@gmail.com>
2022-08-19 21:03:43 -0400

    updates

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 //parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
 const {animals} = require('./data/animals');
 
@@ -112,6 +113,10 @@ app.post(`/api/animals`, (req, res) => {
         const animal = createNewAnimal(req.body, animals);
         res.json(animal);
     }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
